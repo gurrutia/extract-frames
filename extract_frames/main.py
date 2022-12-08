3ea0339321eb8928e98fda67f22f6f7d3268663f
gurrutia <durru7@gmail.com>
2022-12-08 10:16:09 -0500

    added argparse arg frames

--- extract_frames/main.py
+++ extract_frames/main.py
@@ -12,11 +12,31 @@ def valid_path(p: str) -> str:
     return p
 
 
+def positive_int(s: str) -> int:
+    try:
+        v = int(s)
+    except ValueError:
+        raise argparse.ArgumentTypeError(f"Expected integer, got {s!r}")
+
+    if v <= 0:
+        raise argparse.ArgumentTypeError(f"Expected positive integer, got {v}")
+
+    return v
+
+
 def main() -> None:
     parser = argparse.ArgumentParser(
         prog="extract-frames", description="Extract frames from video as .jpg image"
     )
     parser.add_argument("path", type=valid_path, help="path to video with extension")
+    parser.add_argument(
+        "-f",
+        "--frames",
+        type=positive_int,
+        default=1,
+        metavar="",
+        help="split every n frame(s)",
+    )
     args = parser.parse_args()
 
 
